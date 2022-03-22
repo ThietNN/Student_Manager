d81a8df5bddb8bc2d9da8b024de6d3445678dcc2
ThietNN <nguyenthiet1225@gmail.com>
2022-03-22 16:42:58 +0700

    use count(), max(), avg(), etc

--- src/interact.sql
+++ src/interact.sql
@@ -15,3 +15,21 @@ UPDATE student
 SET ClassID = 2
 WHERE StudentName = 'Hung';
 SELECT S.StudentName, Sub.SubjectName, M.Mark FROM STUDENT S JOIN Mark M ON S.studentID = M.StudentId join Subject SUB ON M.SubId = Sub.SubjectID group by Mark desc, StudentName asc;
+
+SELECT Address, COUNT(StudentId) AS 'Số lượng học viên'
+FROM Student
+GROUP BY Address;
+
+SELECT S.StudentId,S.StudentName, AVG(Mark)
+FROM Student S join Mark M on S.StudentId = M.StudentId
+GROUP BY S.StudentId, S.StudentName;
+
+SELECT S.StudentId,S.StudentName, AVG(Mark)
+FROM Student S join Mark M on S.StudentId = M.StudentId
+GROUP BY S.StudentId, S.StudentName
+HAVING AVG(Mark) > 15;
+
+SELECT S.StudentId, S.StudentName, AVG(Mark)
+FROM Student S join Mark M on S.StudentId = M.StudentId
+GROUP BY S.StudentId, S.StudentName
+HAVING AVG(Mark) >= ALL (SELECT AVG(Mark) FROM Mark GROUP BY Mark.StudentId);
